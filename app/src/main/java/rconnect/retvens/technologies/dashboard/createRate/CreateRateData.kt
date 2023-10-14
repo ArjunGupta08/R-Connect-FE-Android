fa40b8d3e1428d66e82728e3abd6cfab895419a9
Arjun Gupta <arjungupta0817@gmail.com>
2023-10-14 17:07:36 +0530

    Nav Drawer and CreateRateTypeFragment Done.

---
+++ app/src/main/java/rconnect/retvens/technologies/dashboard/createRate/CreateRateData.kt
@@ -0,0 +1,8 @@
+package rconnect.retvens.technologies.dashboard.createRate
+
+data class CreateRateData(
+    val meal:String,
+    val day:String,
+    val charge:String,
+    val rate:String,
+)
--- app/src/main/java/rconnect/retvens/technologies/dashboard/createRate/CreateRateTypeFragment.kt
+++ app/src/main/java/rconnect/retvens/technologies/dashboard/createRate/CreateRateTypeFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import rconnect.retvens.technologies.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import rconnect.retvens.technologies.databinding.FragmentCreateRateTypeBinding
+import rconnect.retvens.technologies.databinding.FragmentViewPropertiesBinding
 
 class CreateRateTypeFragment : Fragment() {
+
+    lateinit var binding:FragmentCreateRateTypeBinding
+    val rateList = ArrayList<CreateRateData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +27,38 @@ class CreateRateTypeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_rate_type, container, false)
+        binding = FragmentCreateRateTypeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+//        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+//        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+//        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+//        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+//        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+//        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+//        rateList.add(CreateRateData("Breakfast","Sunday","Per Adult","200"))
+
+
+
+        binding.recyclerInclusion.layoutManager = LinearLayoutManager(requireContext())
+        val createRateTypeAdapter = CreateRateTypeAdapter(requireContext(),rateList)
+        binding.recyclerInclusion.adapter = createRateTypeAdapter
+        createRateTypeAdapter.notifyDataSetChanged()
+
+
+
+
+
+
+
     }
 
 
