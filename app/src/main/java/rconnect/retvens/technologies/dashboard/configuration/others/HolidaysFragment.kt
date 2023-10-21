2af9fb8bc2115b0c103f5335550f8cfb9e1c4acf
Arjun Gupta <arjungupta0817@gmail.com>
2023-10-21 13:19:37 +0530

    dialog_create_payment_type.xml, dialog_create_reservation_type.xml, dialog_create_season.xml, dialog_create_holiday.xml

--- app/src/main/java/rconnect/retvens/technologies/dashboard/configuration/others/HolidaysFragment.kt
+++ app/src/main/java/rconnect/retvens/technologies/dashboard/configuration/others/HolidaysFragment.kt
@@ -1,10 +1,17 @@
 package rconnect.retvens.technologies.dashboard.configuration.others
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.LinearLayoutManager
 import rconnect.retvens.technologies.R
 import rconnect.retvens.technologies.dashboard.configuration.billings.PaymentTypeAdapter
@@ -32,6 +39,42 @@ class HolidaysFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setUpRecycler()
+
+        binding.createNewBtn.setOnClickListener {
+            openCreateNewDialog()
+        }
+
+    }
+
+    private fun openCreateNewDialog() {
+        val dialog = Dialog(requireContext()) // Use 'this' as the context, assuming this code is within an Activity
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(true)
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.setContentView(R.layout.dialog_create_holiday)
+        dialog.window?.apply {
+            setBackgroundDrawableResource(android.R.color.transparent) // Makes the background transparent
+            setLayout(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        }
+
+        val cancel = dialog.findViewById<TextView>(R.id.cancel)
+        val save = dialog.findViewById<CardView>(R.id.saveBtn)
+
+        cancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        save.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.END)
+
+        dialog.show()
 
     }
 
