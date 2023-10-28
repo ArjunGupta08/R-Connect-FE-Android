ebd2119cab3919f776dfe3588e435850ad9e16a4
Arjun Gupta <arjungupta0817@gmail.com>
2023-10-28 10:52:27 +0530

    RatePlanDiscountFragment

---
+++ app/src/main/java/rconnect/retvens/technologies/dashboard/configuration/roomsAndRates/createRate/ratePlaneDiscount/CreateRatePlanAdapter.kt
@@ -0,0 +1,44 @@
+package rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.createRate.ratePlaneDiscount
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import rconnect.retvens.technologies.R
+import rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.createRate.CreateRateData
+
+class CreateRatePlanAdapter(val applicationContext:Context, val rateTypeList:ArrayList<RatePlanDiscountData>):RecyclerView.Adapter<CreateRatePlanAdapter.ViewHolder>() {
+    class ViewHolder(val itemView:View):RecyclerView.ViewHolder(itemView) {
+        val roomType:TextView = itemView.findViewById(R.id.roomType)
+        val selectedCount:TextView = itemView.findViewById(R.id.selectedCount)
+        val recycler:RecyclerView = itemView.findViewById(R.id.recycler)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(applicationContext)
+        val view = inflater.inflate(R.layout.item_applicable_room_type,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return rateTypeList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentData = rateTypeList[position]
+        holder.roomType.text = currentData.roomType
+        holder.selectedCount.text = currentData.selectedCount.toString()
+
+        holder.roomType.setOnClickListener{
+            holder.recycler.visibility = View.VISIBLE
+        }
+
+        holder.recycler.layoutManager=LinearLayoutManager(applicationContext)
+        val adapter = RoomTypeCategoryAdapter(applicationContext, currentData.list)
+        holder.recycler.adapter = adapter
+        adapter.notifyDataSetChanged()
+    }
+}
--- app/src/main/java/rconnect/retvens/technologies/dashboard/configuration/roomsAndRates/createRate/ratePlaneDiscount/RatePlanDiscountFragment.kt
+++ app/src/main/java/rconnect/retvens/technologies/dashboard/configuration/roomsAndRates/createRate/ratePlaneDiscount/RatePlanDiscountFragment.kt
@@ -5,10 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.createRate.CreateRateData
 import rconnect.retvens.technologies.databinding.FragmentRatePlanDiscountBinding
 
 class RatePlanDiscountFragment : Fragment() {
     private lateinit var binding : FragmentRatePlanDiscountBinding
+
+    private val planList = ArrayList<RatePlanDiscountData>()
+    private val roomList = ArrayList<RatePlanRoomType>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +26,19 @@ class RatePlanDiscountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        roomList.add(RatePlanRoomType("Every Day"))
+        roomList.add(RatePlanRoomType("Every Day"))
+        roomList.add(RatePlanRoomType("Every Day"))
+        planList.add(RatePlanDiscountData(1,"Every Day", roomList))
+        planList.add(RatePlanDiscountData(1,"Every Day", arrayListOf()))
+        planList.add(RatePlanDiscountData(1,"Every Day", roomList))
+        planList.add(RatePlanDiscountData(1,"Every Day", arrayListOf()))
+
+        binding.recycler.layoutManager = LinearLayoutManager(requireContext())
+        val createRatePlanAdapter = CreateRatePlanAdapter(requireContext(),planList)
+        binding.recycler.adapter = createRatePlanAdapter
+        createRatePlanAdapter.notifyDataSetChanged()
 
     }
 }
