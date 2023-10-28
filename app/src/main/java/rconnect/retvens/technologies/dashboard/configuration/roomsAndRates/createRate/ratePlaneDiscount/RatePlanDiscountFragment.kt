f685b09bc6fc0c88070d039d39ad893e59e3dfc5
Arjun Gupta <arjungupta0817@gmail.com>
2023-10-28 13:04:22 +0530

    RatePlanDiscountFragment

--- app/src/main/java/rconnect/retvens/technologies/dashboard/configuration/roomsAndRates/createRate/ratePlaneDiscount/RatePlanDiscountFragment.kt
+++ app/src/main/java/rconnect/retvens/technologies/dashboard/configuration/roomsAndRates/createRate/ratePlaneDiscount/RatePlanDiscountFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.card.MaterialCardView
+import rconnect.retvens.technologies.R
 import rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.createRate.CreateRateData
 import rconnect.retvens.technologies.databinding.FragmentRatePlanDiscountBinding
 
@@ -40,5 +44,22 @@ class RatePlanDiscountFragment : Fragment() {
         binding.recycler.adapter = createRatePlanAdapter
         createRatePlanAdapter.notifyDataSetChanged()
 
+        binding.amountCard.setOnClickListener {
+            binding.discountTextLayout.hint = "Discount ₹"
+            cardSelected(binding.amountCard, binding.amountText)
+        }
+        binding.percentCard.setOnClickListener {
+            binding.discountTextLayout.hint = "Discount %"
+            cardSelected(binding.percentCard, binding.percentText)
+        }
+    }
+    private fun cardSelected(card : MaterialCardView, text : TextView){
+        binding.amountCard.strokeColor = ContextCompat.getColor(requireContext(), R.color.textInputStrokeColor)
+        binding.percentCard.strokeColor = ContextCompat.getColor(requireContext(), R.color.textInputStrokeColor)
+        binding.amountText.setTextColor(ContextCompat.getColor(requireContext(), R.color.subtitle))
+        binding.percentText.setTextColor(ContextCompat.getColor(requireContext(), R.color.subtitle))
+
+        card.strokeColor = ContextCompat.getColor(requireContext(), R.color.secondary)
+        text.setTextColor(ContextCompat.getColor(requireContext(), R.color.secondary))
     }
 }
