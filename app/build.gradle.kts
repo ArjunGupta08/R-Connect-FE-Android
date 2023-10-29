b5aaf5d029aecb4c6e599a77f4c7a4d62fb99584
Arjun Gupta <arjungupta0817@gmail.com>
2023-10-29 23:47:05 +0530

    onboarding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation ("com.google.android.gms:play-services-maps:18.2.0")
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation ("com.google.android.libraries.places:places:3.2.0")
+
     implementation("com.google.android.gms:play-services-maps:16.1.0")
     implementation("com.google.android.gms:play-services-location:10.+")
 
