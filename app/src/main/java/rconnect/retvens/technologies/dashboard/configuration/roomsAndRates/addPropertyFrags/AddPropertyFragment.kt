35a137864c3045e7d294d477902ea23894d383d9
Arjun Gupta <arjungupta0817@gmail.com>
2023-11-07 11:21:10 +0530

    Updated RatePlanCompanyFrag, Created item_rate_plan_details.xml

--- app/src/main/java/rconnect/retvens/technologies/dashboard/configuration/roomsAndRates/addPropertyFrags/AddPropertyFragment.kt
+++ app/src/main/java/rconnect/retvens/technologies/dashboard/configuration/roomsAndRates/addPropertyFrags/AddPropertyFragment.kt
@@ -234,6 +234,11 @@ class AddPropertyFragment : Fragment(), OnMapReadyCallback {
             binding.addressAndContacts.setBackgroundDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.corner_top_grey_background))
             binding.propertyImages.setBackgroundDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.corner_top_white_background))
 
+            val childFragment: Fragment = AddImagesFragment()
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.propertyImagesFrameLayout,childFragment)
+            transaction.commit()
+
             binding.propertyProfileLayout.visibility = View.GONE
             binding.propertyImagesFrameLayout.visibility = View.VISIBLE
             binding.addressLayout.visibility = View.GONE
