462b410de6bb7033454684265dee440b1119362e
Arjun Gupta <arjungupta0817@gmail.com>
2023-11-13 12:54:59 +0530

    Amenity Type DropDown with Api

--- app/src/main/java/rconnect/retvens/technologies/Api/DropDownApis.kt
+++ app/src/main/java/rconnect/retvens/technologies/Api/DropDownApis.kt
@@ -3,6 +3,7 @@ package rconnect.retvens.technologies.Api
 import rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.addPropertyFrags.GetPropertyTypeData
 import rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.addPropertyFrags.GetPropertyTypeDataClass
 import rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.addPropertyFrags.GetPropertyTypeRatingData
+import rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.amenity.GetAmenityType
 import rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.createRate.GetRoomType
 import rconnect.retvens.technologies.dashboard.configuration.roomsAndRates.createRate.GetRoomTypeData
 import retrofit2.Call
@@ -11,6 +12,9 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface DropDownApis {
+
+    @GET("getAmenityType")
+    fun getAmenityType (): Call<GetAmenityType>
 
     @GET("getPropertyType")
     fun getPropertyType(): Call<GetPropertyTypeData>
